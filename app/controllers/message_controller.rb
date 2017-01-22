cd8b1155f68bf5a977d9dadf84c9723c4b4a210f
kenjishisei <kenjishisei@yahoo.co.jp>
2017-01-22 19:31:48 +0900

    Define method

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,5 +1,11 @@
 class MessageController < ApplicationController
 
+  before_move: move_to_signed_in
+
   def index
+  end
+
+  def move_to_signed_in
+    redirect_to new_user_session_path uless user_signed_in?
   end
 end
