58b0e73c7873bd4f1f8ccafefa2d82d21445f3cd
kenjishisei <kenjishisei@yahoo.co.jp>
2017-01-23 13:06:25 +0900

    Change sentence

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,11 +1,11 @@
 class MessageController < ApplicationController
 
-  before_move: move_to_signed_in
+  before_action: move_to_signed_in
 
   def index
   end
 
   def move_to_signed_in
-    redirect_to new_user_session_path uless user_signed_in?
+    redirect_to new_user_session_path unless user_signed_in?
   end
 end
