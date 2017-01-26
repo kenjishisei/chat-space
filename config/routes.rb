351d188b9492c60c60d35c31ed258b1ae67b990e
kenjishisei <kenjishisei@yahoo.co.jp>
2017-01-26 20:53:17 +0900

    delete end

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   root 'message#index'
   resources :groups, only: [:new, :create]
   resources :message
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
