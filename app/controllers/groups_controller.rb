63e0402ec8e0383b423faa6f3c30010fc7e264d6
kenjishisei <kenjishisei@yahoo.co.jp>
2017-01-27 19:51:26 +0900

    Define association

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,6 +5,18 @@ class GroupsController < ApplicationController
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.name.present?
+      @group.save
+    redirect_to root_path
+  else
+    render :new
+  end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, user_id:[])
   end
 
 end
