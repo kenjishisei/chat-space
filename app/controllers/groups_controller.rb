dca2532a5055f82da8d1d7b5d73684fc5ce28dac
kenjishisei <kenjishisei@yahoo.co.jp>
2017-01-30 17:58:31 +0900

    change detail

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -30,12 +30,12 @@ class GroupsController < ApplicationController
   end
 
   private
-  
+
   def group_params
     params.require(:group).permit(:name, user_ids:[])
     end
 
-    def set_group
-      @group = Group.find(params[:id])
-    end
+  def set_group
+    @group = Group.find(params[:id])
   end
+end
