3a561ce2a5bfa73e2509d8ace66dd9e11a55ea74
kenjishisei <kenjishisei@yahoo.co.jp>
2017-01-30 17:32:58 +0900

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'message#index'
-  resources :groups, only: [:new, :create]
+  resources :groups, only: [:new, :create, :show, :edit, :update]
   resources :message
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
