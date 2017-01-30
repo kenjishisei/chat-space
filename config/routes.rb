ac9c577c521ce12f72d23e99cddf991e6a409b72
kenjishisei <kenjishisei@yahoo.co.jp>
2017-01-30 19:13:56 +0900

    Change rootes and delete sentence

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'message#index'
-  resources :groups, only: [:new, :create, :show, :edit, :update]
-  resources :message
+  root 'groups#index'
+  resources :groups, except: [:destroy,]
+  resources :message, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
