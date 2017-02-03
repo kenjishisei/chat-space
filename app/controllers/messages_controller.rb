40d2a0bb65e6f07e986e137279654caace14f754
kenjishisei <kenjishisei@yahoo.co.jp>
2017-02-03 20:11:23 +0900

    Message controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,24 @@ class MessagesController < ApplicationController
     @user = User.find(current_user.id)
     @group = Group.find(params[:group_id])
     @groups = @user.groups
+    @message = Message.new
   end
-  
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to group_messages_path(params[:group_id])
+    else
+      flash[:alert] = "メッセージが送信できませんでした。"
+      render "index"
+    end
+  end
+
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body, :image).merge(user_id: current_user.id, group_id: params[:group_id])
+  end
 end
+
