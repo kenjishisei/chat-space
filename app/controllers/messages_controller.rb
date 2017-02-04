fc1485229b97124a9c2931b19073d684e1a51b08
kenjishisei <kenjishisei@yahoo.co.jp>
2017-02-04 12:56:04 +0900

    Create set_group and fix sentence pointed

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,16 +1,14 @@
 class MessagesController < ApplicationController
+  before_action :set_group, only: %i(index create)
 
   def index
-    @user = User.find(current_user.id)
-    @group = Group.find(params[:group_id])
-    @groups = @user.groups
     @message = Message.new
   end
 
   def create
     @message = Message.new(message_params)
     if @message.save
-      redirect_to group_messages_path(params[:group_id])
+      redirect_to group_messages_path(@group)
     else
       flash[:alert] = "メッセージが送信できませんでした。"
       render "index"
@@ -22,6 +20,12 @@ class MessagesController < ApplicationController
 
   def message_params
     params.require(:message).permit(:body, :image).merge(user_id: current_user.id, group_id: params[:group_id])
+  end
+
+  def set_group
+    @user = current_user
+    @group = Group.find(params[:group_id])
+    @groups = @user.groups
   end
 end
 
