5a4b0f72d5b001b5635e3bdc369ca81c10a0155c
kenjishisei <kenjishisei@yahoo.co.jp>
2017-02-05 14:14:32 +0900

    Display group member and non_message sentence

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,13 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def non_message
+    messages.blank? ? "まだメッセージはありません。" : messages.last[:body] ;
+  end
+
+  def  group_member
+    each do |user|
+      user.name
+    end
+  end
 end
