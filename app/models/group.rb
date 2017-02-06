ebbe319311d0a9558fbda6d19b2d7ff8dc6ae920
kenjishisei <kenjishisei@yahoo.co.jp>
2017-02-06 16:51:45 +0900

    Delete sentence(models/groups)

--- app/models/group.rb
+++ app/models/group.rb
@@ -10,9 +10,4 @@ class Group < ApplicationRecord
     messages.blank? ? "まだメッセージはありません。" : messages.last[:body] ;
   end
 
-  def  group_member
-    each do |user|
-      user.name
-    end
-  end
 end
