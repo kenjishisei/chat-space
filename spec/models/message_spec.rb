f48e3514d5d946c5659b9063c1d03cffee382676
kenjishisei <kenjishisei@yahoo.co.jp>
2017-02-06 13:13:18 +0900

    Add sentence in message.rb and message_spec.rb

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,6 +2,15 @@ require 'rails_helper'
 describe Message do
   describe '#create' do
 
-    it "is invalid"
+    it "is invalid without a body" do
+      message = build(:message, body: "")
+      message.valid?
+      expect(message.errors[:body]).to include("can't be blanc")
+    end
+
+    it "is valid with a body" do
+      message = build(:message)
+      expect(message).to be_valid
+    end
   end
 end
