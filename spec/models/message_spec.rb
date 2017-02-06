95725a85f6bf76ae4644facff4c684d787de741e
kenjishisei <kenjishisei@yahoo.co.jp>
2017-02-06 16:54:52 +0900

    Change sentence valid sentence

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 describe Message do
   describe '#create' do
 
+    it "is valid with a body" do
+      message = build(:message)
+      expect(message).to be_valid
+    end
+
     it "is invalid without a body" do
       message = build(:message, body: "")
       message.valid?
       expect(message.errors[:body]).to include("入力されていません")
-    end
-
-    it "is valid with a body" do
-      message = build(:message)
-      expect(message).to be_valid
     end
   end
 end
