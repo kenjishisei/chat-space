e4b6656c3e437bc3bcc78f55f851520a9682b29d
kenjishisei <kenjishisei@yahoo.co.jp>
2017-02-14 17:53:06 +0900

    Set routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'groups#index'
   resources :groups, except: %i(destroy show) do
     resources :messages, only: %i(index create)
   end
+  resources :users, only: %i(index)
+  root 'groups#index'
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
