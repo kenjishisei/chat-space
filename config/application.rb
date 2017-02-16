e2ac3f3a2a6864f398f2469c2dd823304400bb86
kenjishisei <kenjishisei@yahoo.co.jp>
2017-02-16 11:35:21 +0900

    Delete unnecessary sentence

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,6 @@ module ChatSpace
       g.asset false
       g.coffee false
   end
-  config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
